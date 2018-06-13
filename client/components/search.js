00a4273bb459d9a9bb24469463c6c8c0aab3c46e
Jelani <jhankins02@gmail.com>
2018-06-12 19:46:55 -0500

    (begin refactor) TheMovieDB service on click instead of itunes

--- client/components/search.js
+++ client/components/search.js
@@ -1,13 +1,28 @@
-angular.module("movie-shelf")
+angular.module('movie-shelf')
 
-  .component("search", {
+  .component('search', {
     bindings: {
-      result: "<"
+      result: '<',
     },
-    controller: function (itunes) {
+    controller: function controller(itunes, TheMovieDB) {
       this.onClick = (query) => {
         itunes.search(query, this.result);
+        TheMovieDB.search(query)
+          .then((result) => {
+            const { results } = result;
+            TheMovieDB.searchVideos(results[0].id)
+              .then((videos) => {
+                console.log(videos, 'VIDEOs');
+              })
+              .catch((err) => { console.log(err); });
+            TheMovieDB.searchCast(results[0].id)
+              .then((credits) => {
+                console.log(credits, 'Credits');
+              })
+              .catch((err) => { console.log(err); });
+          })
+          .catch((err) => { console.log(err); });
       };
     },
-    templateUrl: "templates/search.html"
+    templateUrl: 'templates/search.html',
   });
