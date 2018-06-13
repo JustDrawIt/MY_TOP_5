f0b9cfa266dc512a66429dc498b50b4bbd78f532
Jelani <jhankins02@gmail.com>
2018-06-12 20:35:48 -0500

    (create) bind getDetails to search component, onclick getDetails from query results

--- client/components/search.js
+++ client/components/search.js
@@ -3,14 +3,16 @@ angular.module('movie-shelf')
   .component('search', {
     bindings: {
       result: '<',
+      getDetails: '<',
     },
     controller: function controller(itunes, TheMovieDB) {
       this.onClick = (query) => {
+        const ctrl = this;
         itunes.search(query, this.result);
         TheMovieDB.search(query)
           .then((result) => {
             const { results } = result;
-            TheMovieDB.getDetailsFromIDs(results);
+            ctrl.getDetails(results);
           })
           .catch((err) => { console.log(err); });
       };
