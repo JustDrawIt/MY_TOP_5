a99835e5218a5e1e136a6a4e849ad5604fa8da03
Jelani <jhankins02@gmail.com>
2018-06-12 20:34:54 -0500

    (create) getDetailsFromIDs in app controller

--- client/components/app.js
+++ client/components/app.js
@@ -1,30 +1,55 @@
 angular.module('movie-shelf')
+  .component('app', {
+    controller: function controller(server, TheMovieDB) {
+      this.movies = [];
+      this.shelf = [];
 
-.component('app', {
-  controller: function(server) {
-    this.movies = [];
-    this.shelf = [];
+      this.searchResults = (data) => {
+        this.movies = data.results;
+        console.log('UPDATED MOVIES', this.movies);
+      };
 
-    this.searchResults = (data) => {
-      this.movies = data.results;
-      console.log('UPDATED MOVIES', this.movies);
-    };
+      this.getDetailsFromIDs = movies => new Promise((resolve, reject) => {
+        console.log('IN RESULTS');
+        let moviesDetails = [];
+        movies.forEach((movie) => {
+          const { id, title } = movie;
+          let movieDetails = {};
+          movieDetails.id = id;
+          movieDetails.title = title;
+          TheMovieDB.searchVideos(id)
+            .then((videos) => {
+              // console.log(videos, 'VIDEOs');
+              movieDetails.videos = videos;
+            })
+            .catch((err) => { console.log(err); });
+          TheMovieDB.searchCast(movies[0].id)
+            .then((credits) => {
+              movieDetails.credits = credits;
+              // console.log(credits, 'Credits');
+            })
+            .catch((err) => { console.log(err); });
+          moviesDetails.push(movieDetails);
+        });
+        console.log(moviesDetails, 'MOVIES DETAILS');
+      });
 
-    this.pushit = (movie) => {
-      this.shelf.unshift(movie);
-      console.log('added to shelf', movie)
-    }
-    this.spliceit = (movie) => {
-      let i = this.shelf.indexOf(movie);
-      this.shelf.splice(i, 1);
-    }
-    // this.pushit = this.pushit.bind(this);
-    // server.getShelf((myMovies) => {
-    //   this.shelf = myMovies;
-    //   console.log('filled your shelf with ', this.shelf);
-    // });
-  },
+      this.pushit = (movie) => {
+        this.shelf.unshift(movie);
+        console.log('added to shelf', movie);
+      };
 
-  templateUrl: '/templates/app.html'
+      this.spliceit = (movie) => {
+        let i = this.shelf.indexOf(movie);
+        this.shelf.splice(i, 1);
+      };
+      // this.pushit = this.pushit.bind(this);
+      // server.getShelf((myMovies) => {
+      //   this.shelf = myMovies;
+      //   console.log('filled your shelf with ', this.shelf);
+      // });
+    },
 
-});
+    templateUrl: '/templates/app.html',
+
+  });
