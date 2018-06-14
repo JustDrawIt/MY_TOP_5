067a9d01ffd3f3aebaa8d3acfa3eefd3743b46a9
Eric O'Neal Jr <ejeric23@gmail.com>
2018-06-14 11:32:09 -0500

    (client) Added the auth function to the login component

--- client/components/login.js
+++ client/components/login.js
@@ -2,18 +2,11 @@ angular.module('movie-shelf')
   .component('login', {
     bindings: {
       authenticated: '<',
+      changeAuth: '<',
     },
     controller: function controller(checkAuth) {
-      const callback = (data) => {
-        console.log(data);
-        if (data) {
-          this.authenticated = true;
-        }
-        console.log(this.authenticated);
-        return this.authenticated;
-      };
       this.onClick = () => {
-        checkAuth.check(callback);
+        checkAuth.check(this.changeAuth);
       };
     },
 
