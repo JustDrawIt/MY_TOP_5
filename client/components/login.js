785922d3f9889549e1a27435e61fa9a8f32dcf82
Eric O'Neal Jr <ejeric23@gmail.com>
2018-06-14 09:35:16 -0500

    (clien) Added login to the app.html

--- client/components/login.js
+++ client/components/login.js
@@ -1,11 +1,14 @@
 angular.module('movie-shelf')
   .component('login', {
-    controller: (checkAuth) => {
-      this.onClick = () => {
-        checkAuth.check();
-      };
-      console.log(checkAuth);
+    bindings: {
+      authenticated: '<',
+    },
+    controller: function(checkAuth) {
+      // this.onClick = () => {
+      //   checkAuth.check();
+      // };
+      // console.log(checkAuth);
     },
 
-    templateURL: '/templates/login.html',
+    templateUrl: '/templates/login.html',
   });
