416af172f1971b5688932c9c3fa2d2a8805a8bcb
Eric O'Neal Jr <ejeric23@gmail.com>
2018-06-15 09:58:20 -0500

    Passing data down to movieListEntry

--- client/components/movieListEntry.js
+++ client/components/movieListEntry.js
@@ -1,11 +1,13 @@
 angular.module('movie-shelf')
   .component('movieListEntry', {
     bindings: {
+      userid: '<',
       movie: '<',
       pushit: '<',
     },
     // finish up this function
     controller: function controller() {
+      console.log(this.userid);
       this.sendMovie = (movie) => {
         console.log(this);
         this.pushit(movie);
