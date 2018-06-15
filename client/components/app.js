57fbd4b8329d600628ab9505147dc6cb75274c1c
Jelani <jhankins02@gmail.com>
2018-06-15 11:41:17 -0500

    (refactor) getDetailsFromIDs

--- client/components/app.js
+++ client/components/app.js
@@ -21,107 +21,18 @@ angular.module('movie-shelf')
       };
 
       this.handleNewUpcoming = () => {
-        this.viewSearched = false;
-        this.viewNewMovies = true;
         TheMovieDB.getUpcoming().then((response) => {
-          response.forEach((movie) => {
-            const {
-              id,
-              title,
-              overview,
-              release_date,
-              poster_path,
-            } = movie;
-            const movieDetails = {};
-            movieDetails.id = id;
-            movieDetails.title = title;
-            movieDetails.overview = overview;
-            movieDetails.releaseDate = release_date;
-            if (poster_path) {
-              movieDetails.posterUrl = `http://image.tmdb.org/t/p/w500${poster_path}`;
-            }
-            TheMovieDB.searchVideos(id)
-              .then((videos) => {
-                const youtubeVids = videos.filter(video => video.site === 'YouTube');
-                if (youtubeVids.length > 0) {
-                  movieDetails.hasMedia = true;
-                }
-                youtubeVids.forEach((video) => {
-                  video.embededLink = $sce.trustAsResourceUrl(`https://www.youtube.com/embed/${video.key}`);
-                });
-                movieDetails.videos = youtubeVids;
-              })
-              .catch((err) => {
-                if (err.data.error === 'Request failed with status code 429') {
-                  M.toast({ html: 'Failed to load all videos, try again in 10seconds' });
-                }
-              });
-            TheMovieDB.searchCast(id)
-              .then((credits) => {
-                const director = credits.crew.filter(member => member.job === 'Director');
-                movieDetails.credits = credits;
-                movieDetails.director = director;
-              })
-              .catch((err) => {
-                if (err.data.error === 'Request failed with status code 429') {
-                  M.toast({ html: 'Failed to load all directors, try again in 10seconds' });
-                }
-              });
-            this.upcoming.push(movieDetails);
-          });
+          this.getDetailsFromIDs(response, 'upcoming', 'viewNewMovies');
         }).catch(err => console.log(err));
         TheMovieDB.getNowPlaying().then((response) => {
-          response.forEach((movie) => {
-            const {
-              id,
-              title,
-              overview,
-              release_date,
-              poster_path,
-            } = movie;
-            const movieDetails = {};
-            movieDetails.id = id;
-            movieDetails.title = title;
-            movieDetails.overview = overview;
-            movieDetails.releaseDate = release_date;
-            if (poster_path) {
-              movieDetails.posterUrl = `http://image.tmdb.org/t/p/w500${poster_path}`;
-            }
-            TheMovieDB.searchVideos(id)
-              .then((videos) => {
-                const youtubeVids = videos.filter(video => video.site === 'YouTube');
-                if (youtubeVids.length > 0) {
-                  movieDetails.hasMedia = true;
-                }
-                youtubeVids.forEach((video) => {
-                  video.embededLink = $sce.trustAsResourceUrl(`https://www.youtube.com/embed/${video.key}`);
-                });
-                movieDetails.videos = youtubeVids;
-              })
-              .catch((err) => {
-                if (err.data.error === 'Request failed with status code 429') {
-                  M.toast({ html: 'Failed to load all videos, try again in 10seconds' });
-                }
-              });
-            TheMovieDB.searchCast(id)
-              .then((credits) => {
-                const director = credits.crew.filter(member => member.job === 'Director');
-                movieDetails.credits = credits;
-                movieDetails.director = director;
-              })
-              .catch((err) => {
-                if (err.data.error === 'Request failed with status code 429') {
-                  M.toast({ html: 'Failed to load all directors, try again in 10seconds' });
-                }
-              });
-            this.nowPlaying.push(movieDetails);
-          });
+          this.getDetailsFromIDs(response, 'nowPlaying', 'viewNewMovies');
         }).catch(err => console.log(err));
       };
 
-      this.getDetailsFromIDs = (movies) => {
-        this.viewSearched = true;
+      this.getDetailsFromIDs = (movies, destination, view) => {
+        this.viewSearched = false;
         this.viewNewMovies = false;
+        this[view] = true;
         // reset moviesDB state;
         this.moviesDB = [];
         // push each movieDetail to moviesDB state
@@ -168,7 +79,7 @@ angular.module('movie-shelf')
                 M.toast({ html: 'Failed to load all directors, try again in 10seconds' });
               }
             });
-          this.moviesDB.push(movieDetails);
+          this[destination].push(movieDetails);
         });
       };
 
